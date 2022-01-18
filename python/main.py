1df9df69c7dea30fd5ecfd99b7370b26f1fd4866
USN-Helgesen <helgesen.haakon@gmail.com>
2022-01-18 13:10:43 +0100

    Experimenting
    Experimenting with the requests library and posting to GraphQL.

--- python/main.py
+++ python/main.py
@@ -1,1 +1,60 @@
+import requests
+import credentials as cred
+from datetime import datetime
 
+target = "https://iot.dimensionfour.io/graph"
+
+"""
+TENANT_ID = cred.TENANT_ID
+TENANT_KEY = cred.TENANT_KEY
+POINT_ID = cred.POINT_ID
+
+
+headers = {
+    "x-tenant-id": TENANT_ID,
+    "x-tenant-key": TENANT_KEY,
+}
+"""
+value = 1212
+timestamp = 2
+
+query = """mutation CREATE_SIGNAL(
+  $timestamp: Timestamp!
+  $pointId: ID!
+  $value: String!
+  ){
+  signal {
+    create(input: {
+      pointId: $pointId
+      signals: [
+        {
+          unit: """ + str(value) + """
+          value: $value
+          type: """ + str(value) + """
+          timestamp: $timestamp
+        }
+      ]
+    }) {
+      id
+      timestamp
+      createdAt
+      pointId
+      unit
+      type
+      data {
+        numericValue
+        rawValue
+      }
+    }
+  }
+}"""
+"""
+json = {
+    "query": query,
+    "variables": {
+        "pointId": POINT_ID,
+        "timestamp": str(timestamp),
+        "value": value,
+    },
+}"""
+print(datetime.now())
