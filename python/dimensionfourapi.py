c4675ed849d968ad13ba497a76bc3d2edf9387f1
USN-Helgesen <helgesen.haakon@gmail.com>
2022-01-21 13:57:38 +0100

    Work on dimensionfourapi functions

---
+++ python/dimensionfourapi.py
@@ -0,0 +1,136 @@
+import requests
+from datetime import datetime
+
+def create_space(name, target, headers):
+    """
+    Function description
+    """
+
+    query = """mutation CREATE_SPACE(
+      $name: String!
+      ){
+      space {
+        create(input: { name: $name}) {
+          id
+          name
+        }
+      }
+    }"""
+    json = {
+        "query": query,
+        "variables":{
+            "name": str(name),
+        },
+    }
+    send_post(target, json, headers)
+
+def create_point(name, space_id, target, headers):
+    """
+
+    """
+    query = """mutation CREATE_POINT {
+      point {
+        create(input: { 
+            spaceId: "<insert_spaceId>"
+            name: "Our device with sensors" 
+        }){
+          id
+        }
+      }
+    }"""
+    json = {
+
+    }
+    send_post(target, json, headers)
+
+def create_signal(value, unit, type, timestamp, point_id, target, headers):
+    """
+
+    """
+    
+    query = """mutation CREATE_SIGNAL(
+      $timestamp: Timestamp!
+      $pointId: ID!
+      $value: String!
+      ){
+      signal {
+        create(input: {
+          pointId: $pointId
+          signals: [
+            {
+              unit: $unit
+              value: $value
+              type: $type
+              timestamp: $timestamp
+            }
+          ]
+        }) {
+          id
+          timestamp
+          createdAt
+          pointId
+          unit
+          type
+          data {
+            numericValue
+            rawValue
+          }
+        }
+      }
+    }"""
+
+    json = {
+        "query": query,
+        "variables": {
+            "pointId": POINT_ID,
+            "timestamp": str(timestamp),
+            "value": str(value),
+            "unit": str(unit),
+            "type": str(type),
+        },
+    }
+    send_post(target, json, headers)
+
+def list_spaces():
+    """
+    
+    """
+    return
+
+def list_points():
+    """
+
+    """
+    return
+
+def list_signals():
+    """
+
+    """
+    return
+
+def retrieve_latest_signal():
+    """
+
+    """
+    return
+
+def send_post(target, json, headers):
+    print("Contacting Dimension Four...")
+    try:
+        res = requests.post(target, json=json, headers=headers)
+    except Exception as e:
+        print(f"Error!: {e}")
+
+    if res.status_code != 200:
+        print("Send error!")
+        print(res.text)
+
+    else:
+        res_json = res.json()
+        if "errors" in res_json.keys():
+            print("Query error!")
+            print(res_json["errors"])
+        else:
+            print("Success!")
+            print(res_json["data"])
--- python/main.py
+++ python/main.py
@@ -1,20 +1,21 @@
 import requests
 import credentials as cred
 from datetime import datetime
+import to_be_named
 
 target = "https://iot.dimensionfour.io/graph"
 
-"""
+
 TENANT_ID = cred.TENANT_ID
 TENANT_KEY = cred.TENANT_KEY
-POINT_ID = cred.POINT_ID
+#POINT_ID = cred.POINT_ID
 
 
 headers = {
     "x-tenant-id": TENANT_ID,
     "x-tenant-key": TENANT_KEY,
 }
-"""
+
 value = 1212
 timestamp = datetime.now()
 
@@ -48,12 +49,14 @@ query = """mutation CREATE_SIGNAL(
     }
   }
 }"""
-
+"""
 json = {
     "query": query,
     "variables": {
-        "pointId": POINT_ID,
         "timestamp": str(timestamp),
         "value": value,
     },
 }
+"""
+b = "hello"
+to_be_named.create_space(b,target,headers)
