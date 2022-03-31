fd4c0fa71d49b34cdd42c139861dbfe04a87d005
USN-Helgesen <helgesen.haakon@gmail.com>
2022-03-31 19:39:13 +0200

    Display for Spaces
    Created a View for Space List, which lists points aswell.

--- ASP_CORE/DimensionFourMonitor/DimensionFourMonitor/Controllers/SpacesController.cs
+++ ASP_CORE/DimensionFourMonitor/DimensionFourMonitor/Controllers/SpacesController.cs
@@ -4,22 +4,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DimensionFourMonitor.Consumers;
+using System.Web;
 
 namespace DimensionFourMonitor.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class SpacesController : ControllerBase
+    public class SpacesController : Controller
     {
         private readonly DFourConsumer _consumer;
         public SpacesController(DFourConsumer consumer)
         {
             _consumer = consumer;
         }
-        public async Task<IActionResult> Get()
+        public async Task<IActionResult> ViewSpacesList()
         {
             var spaces = await _consumer.GetAllSpaces();
-            return Ok(spaces);
+            return View(spaces);
         }
     }
 }
